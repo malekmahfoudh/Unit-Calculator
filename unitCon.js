e9e38820bfbc17a3a510ddef22ac732ffe167195
malekmahfoudh <112383677+malekmahfoudh@users.noreply.github.com>
2023-02-10 12:32:20 +0100

    Working converter (3 decimals)

--- unitCon.js
+++ unitCon.js
@@ -14,17 +14,17 @@ convertBtn.addEventListener("click", function() {
     let baseValue = inputEl.value
     
     lengthEl.textContent = `
-        ${baseValue} meters = ${baseValue * meterToFeet} feet | 
-        ${baseValue} feet = ${baseValue / meterToFeet.toFixed(3)} meters`
+        ${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feet | 
+        ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(3)} meters`
     volumeEl.textContent = `
-        ${baseValue} liters = ${baseValue * literToGallon} gallons | 
-        ${baseValue} gallons = ${baseValue / literToGallon.toFixed(3)} liters`
+        ${baseValue} liters = ${(baseValue * literToGallon).toFixed(3)} gallons | 
+        ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(3)} liters`
     wheightEl.textContent = `
-        ${baseValue} kilos = ${baseValue * kiloToPound} pounds | 
-        ${baseValue} pounds = ${baseValue / kiloToPound.toFixed(3)} kilos`   
+        ${baseValue} kilos = ${(baseValue * kiloToPound).toFixed(3)} pounds | 
+        ${baseValue} pounds = ${(baseValue / kiloToPound).toFixed(3)} kilos`   
     speedEl.textContent = `
-        ${baseValue} KMh = ${baseValue * kmhToMph} MPh | 
-        ${baseValue} MPh = ${baseValue / kmhToMph.toFixed(3)} KMh`   
+        ${baseValue} KMh = ${(baseValue * kmhToMph).toFixed(3)} MPh | 
+        ${baseValue} MPh = ${(baseValue / kmhToMph).toFixed(3)} KMh`   
     
 })
 
