ecc113a5683721309784102250a81df63070e399
malekmahfoudh <112383677+malekmahfoudh@users.noreply.github.com>
2023-02-25 11:21:36 +0100

    final commit

--- unitCon.js
+++ unitCon.js
@@ -4,15 +4,17 @@ let lengthEl = document.querySelector("#length-el")
 let volumeEl = document.querySelector("#volume-el")
 let wheightEl = document.querySelector("#wheight-el")
 let speedEl = document.querySelector("#speed-el")
+let temperatureEl = document.querySelector("#temperature-el")
 
-const meterToFeet =  3.281
-const literToGallon =  0.264
-const kiloToPound =  2.204
+const meterToFeet = 3.281
+const literToGallon = 0.264
+const kiloToPound = 2.204
 const kmhToMph = 0.621
 
-convertBtn.addEventListener("click", function() {
+
+convertBtn.addEventListener("click", function () {
     let baseValue = inputEl.value
-    
+
     lengthEl.textContent = `
         ${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feet | 
         ${baseValue} feet = ${(baseValue / meterToFeet).toFixed(3)} meters`
@@ -21,10 +23,9 @@ convertBtn.addEventListener("click", function() {
         ${baseValue} gallons = ${(baseValue / literToGallon).toFixed(3)} liters`
     wheightEl.textContent = `
         ${baseValue} kilos = ${(baseValue * kiloToPound).toFixed(3)} pounds | 
-        ${baseValue} pounds = ${(baseValue / kiloToPound).toFixed(3)} kilos`   
+        ${baseValue} pounds = ${(baseValue / kiloToPound).toFixed(3)} kilos`
     speedEl.textContent = `
         ${baseValue} KMh = ${(baseValue * kmhToMph).toFixed(3)} MPh | 
-        ${baseValue} MPh = ${(baseValue / kmhToMph).toFixed(3)} KMh`   
-    
+        ${baseValue} MPh = ${(baseValue / kmhToMph).toFixed(3)} KMh`
 })
 
